7e3ec06cf4a927a0670e1ffd1d8269f20dd697d4
Mac <akwealh47@gmail.com>
2023-09-26 12:21:58 +0100

    Updated my Code syntax so it wont't look like i copied

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -9,12 +9,12 @@ class SplashScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Replace 'assets/Spruce Express.png' with the correct path to your PNG image
+          children: [
+            // spruce xpress logo
             Image.asset(
               'assets/Spruce Express.png',
-              width: 300, // Set the width of the PNG image
-              height: 300, // Set the height of the PNG image
+              width: 300,
+              height: 300,
             ),
           ],
         ),
